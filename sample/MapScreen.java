efc8e55f098527a4f29e4ff8d0421dad7e020c58
Jatin Nanda <jatin.nanda1337@gmail.com>
2015-09-20 20:08:14 -0400

    Renamed files.
    Created necessary classes to implement the Map.
    
    Created a game manager to control turn-based gameplay.
    
    For M4, still need to:
     1. Connect the 2d array to the gridpane and enable selection.
     2. Enable player color property marking
     3. Enable land buying phase (just if statements and change the labels to match, and add a pass button to skip turn)

---
+++ sample/MapScreen.java
@@ -0,0 +1,93 @@
+package sample;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by SeYeon on 9/12/2015.
+ */
+public class MapScreen implements Initializable{
+
+    @FXML
+    public static Button townButton;
+
+    @FXML
+    public Button nextPlayer;
+
+    @FXML
+    public static GridPane map;
+
+    @FXML
+    public Label currPlayer;
+
+    @FXML
+    public Label money;
+
+    @FXML
+    public Label ore;
+
+    @FXML
+    public Label energy;
+
+    @FXML
+    public Label food;
+
+    @FXML
+    public Label score;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        GameManager.initializeMap();
+        GameManager.initLandSelection(currPlayer, energy, money, ore, food, score);
+        nextPlayer.setOnAction((event) -> {
+            if (GameManager.currentTurn < GameManager.totalTurnsInitial - 1) {
+                GameManager.currentTurn++;
+                GameManager.initLandSelection(currPlayer, energy, money, ore, food, score);
+            } else {
+                try {
+                    Stage stage;
+                    Parent root;
+                    FXMLLoader loader = new FXMLLoader();
+                    //get reference to the button's stage
+                    stage = (Stage) nextPlayer.getScene().getWindow();
+                    //load up other FXML document
+                    root = loader.load(getClass().getResource("Map2.fxml"));
+                    //create a new scene with root and set the stage
+                    Scene scene = new Scene(root, 800, 700);
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    System.out.println("Pls");
+                }
+            }
+        });
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("TownOptions.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Town Actions");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+    }
+}
---
+++ sample/Player.java
@@ -0,0 +1,87 @@
+package sample;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Created by jatin1 on 9/18/15.
+ */
+public class Player implements Comparable<Player> {
+    private String name;
+    private int foodCount;
+    private int energyCount;
+    private int money;
+    private int landCount;
+    private int oreCount;
+    private Race race;
+    private Color color;
+    private int score = energyCount + foodCount + 500 * landCount + money;
+
+    public Player(String name, Race race, Color color) {
+        this.name = name;
+        this.race = race;
+        this.color = color;
+        if (GameManager.difficulty.equals("Beginner")) {
+            foodCount = 8;
+            energyCount = 4;
+        } else {
+            foodCount = 4;
+            energyCount = 2;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFoodCount() {
+        return foodCount;
+    }
+
+    public int getEnergyCount() {
+        return energyCount;
+    }
+
+    public int getOreCount() {
+        return oreCount;
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getLandCount() {
+        return landCount;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setEnergyCount(int energyCount) {
+        this.energyCount = energyCount;
+    }
+
+    public void setFoodCount(int foodCount) {
+        this.foodCount = foodCount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLandCount(int landCount) {
+        this.landCount = landCount;
+    }
+
+    public int compareTo(Player other) {
+        return this.score - other.getScore();
+    }
+}
