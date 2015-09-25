b67fd8a44c41be0ce84eecee5cb06acd0dfe6c40
Jatin Nanda <jatin.nanda1337@gmail.com>
2015-09-25 04:37:50 -0400

    Fixed the 2D array

--- M4/GameManager.java
+++ M4/GameManager.java
@@ -26,11 +26,11 @@ public class GameManager {
                 if (i == 2 && j == 4) {
                 } else if (j == 4) {
                     gameMap[i][j] = TileType.RIVER;
-                } else if ((i == 0 && j == 2) || (i == 1 && j == 1) && (i == 2 && j == 8)) {
+                } else if ((i == 0 && j == 2) || (i == 1 && j == 1) || (i == 2 && j == 8)) {
                     gameMap[i][j] = TileType.MOUNTAIN1;
-                } else if ((i == 3 && j == 1) || (i == 3 && j == 6) && (i == 4 && j == 2) && (i == 4 && j == 8)) {
+                } else if ((i == 3 && j == 1) || (i == 3 && j == 6) || (i == 4 && j == 2) && (i == 4 && j == 8)) {
                     gameMap[i][j] = TileType.MOUNTAIN2;
-                } else if ((i == 0 && j == 6) || (i == 1 && j == 8) && (i == 2 && j == 0)) {
+                } else if ((i == 0 && j == 6) || (i == 1 && j == 8) || (i == 2 && j == 0)) {
                     gameMap[i][j] = TileType.MOUNTAIN3;
                 } else {
                     gameMap[i][j] = TileType.PLAIN;
